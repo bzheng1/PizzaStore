99e3e797aa6cd1fb23a160754a9a3e6e02b607c6
Bryan Zheng <bzheng@uno.edu>
2018-03-19 11:49:06 -0500

    added constructor

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,42 @@
 import java.util.concurrent.Semaphore;
-
+class PersonConstructor{
+    Semaphore personSem = new Semaphore(1);
+    String letter;
+    PersonConstructor(String letter, Semaphore personSem){
+        this.letter = letter;
+        this.personSem = personSem;
+    }
+}
 public class Main {
-    private static volatile Semaphore qcd = new Semaphore(1);
-
-
+    private static Semaphore personSem = new Semaphore(1);
     public static void main(String[] args) {
 	// write your code her
+        PersonConstructor PersonA = new PersonConstructor("PersonA", personSem);
+        PersonConstructor PersonB = new PersonConstructor("PersonB", personSem);
+        PersonConstructor PersonC = new PersonConstructor("PersonC", personSem);
 
 
     }
-
+    public static void run(){
+        try{
+            personSem.acquire();
+            ordering();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            personSem.release();
+        }
+    }
     public static void ordering(){
-        System.out.println(Thread.currentThread().getName() + "is ordering.");
-
-    }
-
-    public static void cooking(){
-        System.out.println(Thread.currentThread().getName() + "is being cooked");
+        System.out.println(Thread.currentThread().getName() + " is being read and placed in oven.");
+        personSem.release();
     }
 
     public static void handOver(){
-        System.out.println(Thread.currentThread().getName() + "is being handed over.");
+        System.out.println(Thread.currentThread().getName() + "is being handed over to quality control.");
     }
-
-    new Thread
+    public static void finished(){
+        System.out.println(Thread.currentThread().getName() + "is being handed over to customer.");
+    }
 
 }
