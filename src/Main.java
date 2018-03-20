a7fd818f54a30ad8dba9aee36be5a144ca1b55bb
Bryan Zheng <bzheng@uno.edu>
2018-03-20 14:29:21 -0500

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,48 @@ class PersonConstructor extends Thread{
 
 
 @Override
-public void run(){
-    try {
-        System.out.println(letter + "is reading and placing pizza in the oven");
-    }finally {
-
+    public void run(){
+        try {
+            if (letter.equals("PersonA")) {
+                personSem.acquire();
+                System.out.println(letter + " is reading and placing pizza in the oven");
+            }else if(letter.equals("PersonB")) {
+                try {
+                    personSem.acquire();
+                    System.out.println(letter + " is checking the pizza and handing the pizza over to person B");
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }personSem.release();
+            }else if(letter.equals("PersonC")) {
+                try {
+                    personSem.acquire();
+                    System.out.println(letter + " is delivering the pizza.");
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }personSem.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }personSem.release();
     }
-}
 }
 
 
 public class Main  {
     public static Semaphore personSem = new Semaphore(1);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // write your code here
         PersonConstructor PersonA = new PersonConstructor("PersonA", personSem);
         PersonConstructor PersonB = new PersonConstructor("PersonB", personSem);
         PersonConstructor PersonC = new PersonConstructor("PersonC", personSem);
 
+        PersonA.start();
+        PersonB.start();
+        PersonC.start();
+        PersonA.join();
+        PersonB.join();
+        PersonC.join();
+        //System.exit(0);
     }
 
 
